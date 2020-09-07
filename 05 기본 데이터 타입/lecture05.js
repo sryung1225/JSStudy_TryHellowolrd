2b6bcbf6322929c4ea2b64745298334b739fbc8f
sryung1225 <sryung1225@gmail.com>
2020-09-07 17:22:46 +0900

    05 기본 데이터 타입

---
+++ 05 기본 데이터 타입/lecture05.js
@@ -0,0 +1,10 @@
+var a=100, b=3.14; //number
+var c="안녕하세요", d="a"; //string
+var e=true, f=false; //boolean
+
+console.log(a, typeof(a));
+console.log(b, typeof(b));
+console.log(c, typeof(c));
+console.log(d, typeof(d));
+console.log(e, typeof(e));
+console.log(f, typeof(f));
---
+++ 05-실습 기본 데이터 타입 실습하기/practice.js
@@ -0,0 +1,11 @@
+/* step 1 */
+var n=1;
+console.log(typeof(n));
+
+/* step 2 */
+var b=true;
+console.log(typeof(b));
+
+/* step 3 */
+var s="문자열입니다."
+console.log(typeof(s));
