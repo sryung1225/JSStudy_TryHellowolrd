32cce3513803181047efd0cf144a02d5c93ee3f8
sryung1225 <sryung1225@gmail.com>
2020-09-07 17:14:00 +0900

    04 변수 활용하기

---
+++ 04 변수 활용하기/lecture04.js
@@ -0,0 +1,7 @@
+var msg; //변수 msg 선언
+msg="Hello World";
+alert(msg);
+console.log(msg);
+
+var name=prompt("이름을 입력해주세요.");
+console.log(name, " 님 환영합니다.");
---
+++ 04-실습 변수 사용하기/practice02.js
@@ -0,0 +1,2 @@
+var msg="출력될 문자열입니다.";
+console.log(msg);
