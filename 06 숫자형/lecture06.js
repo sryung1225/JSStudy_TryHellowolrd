7a4a325b78cb5d03d113ca10fa013fed5a95c31e
sryung1225 <sryung1225@gmail.com>
2020-09-07 19:25:02 +0900

    06 숫자형

---
+++ 06 숫자형/lecture06.js
@@ -0,0 +1,9 @@
+var height=prompt("키를 입력해 주세요."); //string(문자열)
+console.log(height, typeof(height));
+
+
+var height_int=parseInt(height); //int로 변환
+console.log(height_int, typeof(height_int));
+
+var height_float=parseFloat(height); //float으로 변환
+console.log(height_float, typeof(height_float));
---
+++ 06-숫자형 실습하기/practice.js
@@ -0,0 +1,11 @@
+var str="3.141592는 Pi의 근삿값입니다.";
+
+/* step 1 */
+var pi_int;
+pi_int=parseInt(str);
+console.log(pi_int);
+
+/* step 2 */
+var pi_float;
+pi_float=parseFloat(str);
+console.log(pi_float);
