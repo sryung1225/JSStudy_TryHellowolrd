048cc556e5ea018e3208c36e7da76abc6bc9be62
sryung1225 <sryung1225@gmail.com>
2020-09-09 01:20:46 +0900

    10 산술 연산자

---
+++ 10 산술 연산자/lecture10.js
@@ -0,0 +1,23 @@
+var a=1;
+console.log(a); //1
+console.log(a+1); //2
+console.log(a); //1
+
+var b=1;
+console.log(b); //1
+console.log(++b); //2
+console.log(b); //2
+
+var c=1;
+console.log(c); //1
+console.log(--c); //0
+console.log(c); //0
+
+var d=1;
+console.log(d); //1
+console.log(d++); //1
+console.log(d); //2
+
+console.log(Math.pow(2, 3)); //2의 3제곱 = 8
+console.log(Math.sqrt(16)); //루트16 = 4
+console.log(Math.random()); //0~1 사이 랜덤 수
